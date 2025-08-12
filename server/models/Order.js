f31a977ca81acf8cc4aa1264fea1d8a619c2e72a
bckspce <49206472+pwamoreno@users.noreply.github.com>
2025-08-12 11:21:40 +0100

    payment start

--- server/models/Order.js
+++ server/models/Order.js
@@ -1,8 +1,33 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
 const OrderSchema = new mongoose.Schema({
-    userId: String,
-    cartItems: [
-        
-    ]
-})
+  userId: String,
+  cartItems: [
+    {
+      productId: String,
+      title: String,
+      image: String,
+      price: String,
+      salePrice: String,
+      quantity: Number,
+    },
+  ],
+  addressInfo: {
+    addressId: String,
+    address: String,
+    city: String,
+    pincode: String,
+    phone: String,
+    notes: String,
+  },
+  orderStatus: String,
+  paymentMethod: String,
+  PaymentStatus: String,
+  totalAmount: Number,
+  orderDate: Date,
+  orderUpdateDate: Date,
+  paymentId: String,
+  payerId: String,
+});
+
+module.exports = mongoose.model("Order", OrderSchema);
